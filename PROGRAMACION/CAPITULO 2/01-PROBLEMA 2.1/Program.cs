d1d83a059982b2973ffc8437cd6ebafcdce070ab
Fernando <desarrollostraps@gmail.com>
2021-09-02 22:15:19 -0500

    cuestionario y codigo

---
+++ PROGRAMACION/CAPITULO 2/01-PROBLEMA 2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01_PROBLEMA_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int costoInicial = 20000;
+            int costoRecuperacion = 2000;
+            int anio = 2005;
+            int numerodeAnios = 6;
+            decimal depreciacionAnual = (costoInicial - costoRecuperacion) / numerodeAnios;
+            decimal depreciacionAcumulada = 0;
+            decimal valorAnual = costoInicial;
+            int contador = 1;
+            Console.WriteLine("Año       Depreciacion          Depreciacion Acumulada        Valor Anual");
+
+            while(contador <= numerodeAnios)
+        {
+         
+         anio = anio +1;
+         depreciacionAcumulada = depreciacionAcumulada + depreciacionAnual;
+         valorAnual = valorAnual - depreciacionAnual;
+         
+         Console.WriteLine("(0) ({1})           {2}         {3}         {4}"   ,contador, anio, depreciacionAnual, depreciacionAcumulada, valorAnual);
+         contador = contador +1;   
+        }
+
+        }
+    }
+}
