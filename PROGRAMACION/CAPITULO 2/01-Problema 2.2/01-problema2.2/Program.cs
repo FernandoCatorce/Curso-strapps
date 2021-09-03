19a9a9d715729e0828d6db7aa87a1364ab111f55
Fernando <desarrollostraps@gmail.com>
2021-09-03 12:51:01 -0500

    cambios relocos

---
+++ PROGRAMACION/CAPITULO 2/01-Problema 2.2/01-problema2.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_problema2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
